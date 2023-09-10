1af699e77488afc418b373943b9e64cca216db71
João Fuzeto <fuzeto.jf@gmail.com>
2023-09-11 00:52:13 +0100

    Integrating this API with Twilio API, creating Service Class responsible for sending messages through SMS

--- src/main/java/com/messaging/producer/QueueProducer.java
+++ src/main/java/com/messaging/producer/QueueProducer.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
